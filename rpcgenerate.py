b299ca460e060a8cfa7a477a7ba2413fd30d072e
git <ravi.serota@protonmail.com>
2019-11-19 22:47:31 -0500

    ready to do final testing

--- rpcgenerate.py
+++ rpcgenerate.py
@@ -60,7 +60,6 @@ def createFunctionsList(decls):
 	return functionList
 
 def createTypesDict(decls):
-
 	return decls["types"]
 
 def createFile(fileName):
@@ -77,8 +76,6 @@ def createFile(fileName):
 
 	with open(fileStub, 'w+') as file:
 		file.write(headers % ("stub", fileName))
-
-
 
 def writeProxy(typeDict, functionList):
 
@@ -160,9 +157,12 @@ def createArg(arg, typeDict):
 	elif arg["type"] == "string":
 		ret += stringCreateArg(arg["name"])
 	elif typeDict[arg["type"]]["type_of_type"] == "array":
-		ret += arrayCreateArg(arg, typeDict)
+		ret += arrayCreateArg(arg, typeDict, arg["name"])
+	elif typeDict[arg["type"]]["type_of_type"] == "struct":
+		ret += structCreateArg(arg, typeDict, arg["name"])
 	else:
-		print "in else"
+		print "Argument is not of known type"
+		
 	return ret
 
 def numCreateArg(argName):
@@ -170,6 +170,7 @@ def numCreateArg(argName):
 	nobracketsArgName = argName.replace('[', '').replace(']', '')
 	writeString += "\tstring %sStr = to_string(%s);\n" % (nobracketsArgName, argName)
 	writeString += writeArg(nobracketsArgName)
+	writeString += "\n"
 	return writeString
 
 def stringCreateArg(argName):
@@ -177,23 +178,55 @@ def stringCreateArg(argName):
 	nobracketsArgName = argName.replace('[', '').replace(']', '')
 	writeString += "\tstring %sStr = %s;\n" % (nobracketsArgName, argName)
 	writeString += writeArg(nobracketsArgName)
+	writeString += "\n"
 	return writeString
 
-def arrayCreateArg(arg, typeDict):
+def arrayCreateArg(arg, typeDict, name):
 	writeString = ""
 	typeObj = typeDict[arg["type"]]
 	memberType = typeObj["member_type"]
 	elementCount = int(typeObj["element_count"])
-	arrayName = arg["name"]
+	arrayName = name
 	for i in range(elementCount):
-		newName = arg["name"] + "[" + str(i) + "]";
+		newName = arrayName + "[" + str(i) + "]";
 		if memberType == "int" or memberType == "float":
 			writeString += numCreateArg(newName)
 		elif memberType == "string":
 			writeString += stringCreateArg(newName)
-		else:
-			print "struct or array in array"
-		
+		elif memberType[:1] == "_":
+			writeString += nDArrayArg(newName, memberType)
+	return writeString
+
+def structCreateArg(arg, typeDict, name):
+	writeString = ""
+	typeObj = typeDict[arg["type"]]
+	structMembers = typeObj["members"]
+	structName = arg["name"]
+	for member in structMembers:
+		if member["type"] == "int" or member["type"] == "float":
+			writeString += numCreateArg(structName + "." + member["name"])
+		elif member["type"] == "string":
+			writeString += stringCreateArg(structName + "." + member["name"])
+		elif typeDict[member["type"]]["type_of_type"] == "array":
+			writeString += arrayCreateArg(member, typeDict, structName + "." + member["name"])
+		elif typeDict[member["type"]]["type_of_type"] == "struct":
+			writeString += structCreateArg(member, typeDict, structName + "." + member["name"])
+
+	return writeString
+
+def nDArrayArg(newName, memberType):
+	writeString = ""
+	firstBracket = memberType[memberType.find('['):][1:]
+	elementCount = firstBracket[:firstBracket.find(']')]
+	arrayType = memberType.split('[')[0][2:]
+	for i in range(int(elementCount)):
+		if firstBracket.find("[") != -1:
+			writeString += nDArrayArg((newName+"["+str(i)+"]"), "__"+arrayType+firstBracket[firstBracket.find(']'):][1:])
+			continue
+		if arrayType == "int" or memberType == "float":
+			writeString += numCreateArg(newName+"["+str(i)+"]")
+		elif arrayType == "string":
+			writeString += stringCreateArg(newName+"["+str(i)+"]")
 	return writeString
 
 def writeArg(argName):
@@ -224,107 +257,288 @@ def voidCreateReturn(funcName):
 def writeStub(typeDict, functionList):
 
 	writeString = ""
-
 	### Create declaration for function stub
 	for func in functionList:
-		writeString = "void __%s(" % func[1]
-		writeString += ") {\n"
-
-		### Create read buffer
-		if func[0] == "void":
-			writeString += "\tchar doneBuffer[5] = \"DONE\";\n\n"
-		
-		if len(func[2]) > 0:
-			writeString += "\tchar readBuffer[512];\n\n"
-
-		for arg in func[2]:
-			if typeDict[arg["type"]]["type_of_type"] == "array":
-				writeString += "\tstring temp;\n"
-				break
-
-		### Declare argument variables
-		for arg in func[2]:
-			writeString += stubCreateArg(arg, typeDict)
-
-
-		### Declare return variable
-		if func[0] != "void":
-			writeString += "\t%s ret;\n" % (func[0])
-
-		### Declare length variable for parsing readBuffer
-		if len(func[2]) > 0:
-			writeString += "\tint readLen = 0;\n"
-
-		writeString += "\n"
-		writeString += "\t//\n\t// Time to actually call the function\n\t//\n"
-
-		writeString += "\tc150debug->printf(C150RPCDEBUG,\"%s: invoking %s()\");\n" % (fileProxy, func[1])
-
-		writeString += "\tusleep(250000);\n"
-
-		if len(func[2]) > 0:
-			writeString += "\tRPCSTUBSOCKET->read(readBuffer, sizeof(readBuffer));\n\n"
-
-
-		### Loop through arguments
-		first = True;
-		for arg in func[2]:
-			typeString = arg["type"]
-			name = arg["name"]
-			if typeString == "int":
-				# ret is tuple (string, bool)
-				ret = intToArgType(name, first)
-				writeString += ret[0]
-				first = ret[1]
-			elif typeString == "float":
-				ret = floatToArgType(name, first)
-				writeString += ret[0]
-				first = ret[1]
-			elif typeString == "string":
-				ret = stringToArgType(name, first)
-				writeString += ret[0]
-				first = ret[1]
-			elif typeDict[typeString]["type_of_type"] == "array":
-				ret = arrayToArgType(first, arg, typeDict)
-				writeString += ret[0]
-				first = ret[1]
-
-		firstArg = True
-		if func[0] == "void":
-			writeString += "\t%s(" % (func[1])
-		else:
-			writeString += "\tret = %s(" % (func[1])
-		for arg in func[2]:
-			if firstArg:
-				writeString = writeString + "%s" % (arg["name"])
-				firstArg = False
-			else:
-				writeString = writeString + ", %s" % (arg["name"])
-		writeString = writeString + ");\n"
-
-
-
-		if func[0] == "int" or func[0] == "float":
-			writeString += "\tstring retStr = to_string(ret);\n"
-		elif func[0] == "string":
-			writeString += "\tstring retStr = ret;\n"
-		else:
-			pass
-
-		writeString += "\tc150debug->printf(C150RPCDEBUG,\"%s: returned from %s() -- responding to client\");\n" % (fileStub, func[1])
-
-		if func[0] == "void":
-			writeString += "\tRPCSTUBSOCKET->write(doneBuffer, strlen(doneBuffer)+1);\n"
-		else: 
-			writeString += "\tRPCSTUBSOCKET->write(retStr.c_str(), retStr.length()+1);\n"
-		writeString += "}\n\n"
-
-
-		with open(fileStub, "a") as file:
-			file.write(writeString)
+		writeString += createStubFunction(func)
 
 	with open(fileStub, "a") as file:
+		file.write(writeString)
 		file.write(writeSupportFuncs(functionList))
+
+def createStubFunction(func):
+	writeString = "void __%s(" % func[1]
+	writeString += ") {\n"
+
+	### Create read buffer
+	if len(func[2]) > 0:
+		writeString += "\tchar readBuffer[512];\n\n"
+
+	writeString += stubCreateArgDecls(func[2], typeDict)
+	writeString += createReturnDecl(func[0])
+	
+	### Declare length variable for parsing readBuffer
+	if len(func[2]) > 1:
+		writeString += "\tint readLen = 0;\n"
+
+	writeString += "\n"
+	writeString += "\t//\n\t// Time to actually call the function\n\t//\n"
+	writeString += "\tc150debug->printf(C150RPCDEBUG,\"%s: invoking %s()\");\n" % (fileProxy, func[1])
+	writeString += "\tusleep(250000);\n"
+
+	if len(func[2]) > 0:
+		writeString += "\tRPCSTUBSOCKET->read(readBuffer, sizeof(readBuffer));\n\n"
+
+	writeString += readArgsFromNetwork(func[2])
+	writeString += createFunctionCall(func)
+	writeString += createReturnStatement(func[0], "ret", typeDict)
+
+	writeString += "\tc150debug->printf(C150RPCDEBUG,\"%s: returned from %s() -- responding to client\");\n" % (fileStub, func[1])
+	writeString += "}\n\n"
+
+	return writeString
+
+def stubCreateArgDecls(args, typeDict):
+	writeString = ""
+	for arg in args:
+		writeString += stubCreateArgDecl(arg, typeDict)
+	return writeString
+
+def stubCreateArgDecl(arg, typeDict):
+	writeString = ""
+	typeOfType = typeDict[arg["type"]]["type_of_type"]
+	if typeOfType == "builtin":
+		writeString += "\t%s %s;\n" % (arg["type"], arg["name"])
+	elif typeOfType == "array":
+		memberType = typeDict[arg["type"]]["member_type"]
+		bracketString = "[%s]" % typeDict[arg["type"]]["element_count"]
+		while typeDict[memberType]["type_of_type"] == "array":
+			bracketString += "[%s]" % typeDict[memberType]["element_count"]
+			memberType = typeDict[memberType]["member_type"]
+		baseType = memberType
+		writeString += "\t%s %s%s;\n" % (baseType, arg["name"], bracketString)
+	elif typeOfType == "struct":
+		writeString += "\tstruct %s %s;\n" % (arg["type"], arg["name"])
+	return writeString
+
+def createReturnDecl(returnType):
+	writeString = ""
+	if returnType != "void":
+		if typeDict[returnType]["type_of_type"] == "struct":
+			writeString += "\tstruct %s ret;\n" % returnType
+		else:
+			writeString += "\t%s ret;\n" % returnType
+	return writeString
+
+def readArgsFromNetwork(args):
+	writeString = "\t//\n\t// Read args from readBuffer\n\t//\n"
+	first = True;
+	for arg in args:
+		typeString = arg["type"]
+		name = arg["name"]
+		if typeString == "int":
+			# ret is tuple (string, bool)
+			ret = readInt(name, first, 1)
+			writeString += ret[0]
+			first = ret[1]
+		elif typeString == "float":
+			ret = readFloat(name, first, 1)
+			writeString += ret[0]
+			first = ret[1]
+		elif typeString == "string":
+			ret = readString(name, first, 1)
+			writeString += ret[0]
+			first = ret[1]
+		elif typeDict[typeString]["type_of_type"] == "array":
+			ret = readArray(first, arg["type"], arg["name"], typeDict)
+			writeString += ret[0]
+			first = ret[1]
+		elif typeDict[typeString]["type_of_type"] == "struct":
+			ret = readStruct(first, arg["type"], arg["name"], typeDict)
+			writeString += ret[0]
+			first = ret[1]
+
+	writeString += "\n"
+
+	return writeString
+
+def createReturnStatement(retType, retName, typeDict):
+	writeString = ""
+	if retType == "void":
+		writeString += createVoidReturn()
+	elif retType == "int":
+		writeString += createIntReturn(retName, retName)
+	elif retType == "float":
+		writeString += createFloatReturn(retName, retName)
+	elif retType == "string":
+		writeString += createStringReturn(retName, retName)
+	elif typeDict[retType]["type_of_type"] == "struct":
+		writeString += createStructReturn(retType, retName, typeDict)
+	else:
+		print("Error: Bad return type")
+
+	return writeString
+
+def createVoidReturn():
+	writeString = "\tstring done = \"DONE\";\n"
+	writeString += "\tRPCSTUBSOCKET->write(done, done.length()+1);\n"
+	return writeString
+
+def createIntReturn(retName, varName):
+	return "\tstring %sStr = to_string(%s);\n" % (retName, varName)
+
+def createFloatReturn(retName, varName):
+	return "\tstring %sStr = to_string(%s);\n" % (retName, varName)
+
+def createStringReturn(retName, varName):
+	return "\tstring %sStr = %s;\n" % (retName, varName)
+
+def createStructReturn(retType, retName, typeDict):
+	# To do: change fullName variable name
+	writeString = ""
+	for m in typeDict[retType]["members"]:
+		mName = m["name"]
+		mType = m["type"]
+		strName = "%s%s" % (retName, mName)
+		fullName = "%s.%s" % (retName, mName)
+		if mType == "int":
+			writeString += createIntReturn(strName, fullName)
+		elif mType == "float":
+			writeString += createFloatReturn(strName, fullName)
+		elif mType == "string":
+			writeString += createStringReturn(strName, fullName)
+		elif typeDict[mType]["type_of_type"] == "array":
+			writeString += createArrayReturn(mType, fullName, typeDict)
+		elif typeDict[mType]["type_of_type"] == "struct":
+			writeString += createStructReturn(mType, fullName, typeDict)
+		# RPCSOCKETWRITE
+		writeString += "\tRPCSTUBSOCKET->write(%s.c_str(), %s.length()+1);\n\n" % (strName, strName)
+	return writeString
+
+def createArrayReturn(retType, retName, typeDict):
+	writeString = ""
+	memberType = typeDict[retType]["member_type"]
+	elemCount  = typeDict[retType]["element_count"]
+
+	writeString += "\tstring temp;\n"
+	depth = 1
+	while typeDict[memberType]["type_of_type"] == "array":
+		depth += 1
+		memberType = typeDict[memberType]["member_type"]
+	writeString += arrayNDToArgType(depth, argType, argName, typeDict)
+
+	bracketString = "%s[i]" % argName
+	for i in range(depth - 1):
+		bracketString += "[%s]" % chr(ord('i') + (i + 1))
+	
+	if memberType == "int":
+		writeString += createIntReturn(bracketString, bracketString)
+	elif memberType == "float":
+		writeString += createFloatReturn(bracketString, bracketString)
+	elif memberType == "string":
+		writeString += createStringReturn(bracketString, bracketString)
+	elif typeDict[memberType]["type_of_type"] == "struct":
+		writeString += createStructReturn(typeDict[memberType], bracketString, typeDict)
+
+	for i in range(depth, 0, -1):
+		writeString += "\t" * i + "}\n"
+
+	return (writeString, False)
+
+def arrayNDToArgType(depth, argType, argName, typeDict):
+	writeString = ""
+	iterVar = 'i'
+	for i in range(depth):
+		elementCount = typeDict[argType]["element_count"]
+		writeString += '\t' * (i + 1)
+		writeString += "for (int %s=0; %s<%s; %s++) {\n" % (iterVar, iterVar, elementCount, iterVar)
+		iterVar = chr(ord(iterVar) + 1)
+		argType = typeDict[argType]["member_type"]
+	return writeString
+
+def createFunctionCall(func):
+	writeString = ""
+	firstArg = True
+	if func[0] == "void":
+		writeString += "\t%s(" % (func[1])
+	else:
+		writeString += "\tret = %s(" % (func[1])
+	for arg in func[2]:
+		if firstArg:
+			writeString += "%s" % (arg["name"])
+			firstArg = False
+		else:
+			writeString += ", %s" % (arg["name"])
+	writeString += ");\n"
+
+	return writeString
+
+def readInt(argName, first, tabs):
+	writeString = "\t" * tabs + "%s = stoi(string(&(readBuffer[readLen])));\n" % (argName)
+	writeString += "\t" * tabs + "readLen += to_string(%s).length()+1;\n\n" % (argName)
+	return (writeString, False)
+
+def readFloat(argName, first, tabs):
+	writeString = "\t" * tabs + "%s = stof(string(&(readBuffer[readLen])));\n" % (argName)
+	writeString += "\t" * tabs + "readLen += to_string(%s).length()+1;\n\n" % (argName)
+	return (writeString, False)
+
+def readString(argName, first, tabs):
+	writeString = "\t" * tabs + "%s = &(readBuffer[readLen]);\n" % (argName)
+	writeString += "\t" * tabs + "readLen += %s.length()+1;\n\n" % (argName)
+	return (writeString, False)
+
+def readStruct(first, argType, argName, typeDict):
+	members = typeDict[argType]["members"]
+	writeString = ""
+	first = False
+	for m in members:
+		mType = m["type"]
+		mName = m["name"]
+		fullName = "%s.%s" % (argName, mName)
+		if mType == "int":
+			writeString += readInt(fullName, first, 2)[0]
+		elif mType == "float":
+			writeString += readFloat(fullName, first, 2)[0]
+		elif mType == "string":
+			writeString += readString(fullName, first, 2)[0]
+		else:
+			typeOfType = typeDict[mType]["type_of_type"]
+			if typeOfType == "array":
+				writeString += readArray(first, mType, fullName, typeDict)[0]
+			elif typeOfType == "struct":
+				writeString += readStruct(first, mType, fullName, typeDict)[0]	
+
+	return (writeString, False)
+
+def readArray(first, argType, argName, typeDict):
+	writeString = ""
+	typeObj = typeDict[argType]
+	memberType = typeObj["member_type"]
+
+	writeString += "\tstring temp;\n"
+	depth = 1
+	while typeDict[memberType]["type_of_type"] == "array":
+		depth += 1
+		memberType = typeDict[memberType]["member_type"]
+	writeString += arrayNDToArgType(depth, argType, argName, typeDict)
+
+	bracketString = "%s[i]" % argName
+	for i in range(depth - 1):
+		bracketString += "[%s]" % chr(ord('i') + (i + 1))
+	
+	if memberType == "int":
+		writeString += readInt(bracketString, first, depth)[0]
+	elif memberType == "float":
+		writeString += readFloat(bracketString, first, depth)[0]
+	elif memberType == "string":
+		writeString += readString(bracketString, first, depth)[0]
+	elif typeDict[memberType]["type_of_type"] == "struct":
+		writeString += readStruct(first, memberType, bracketString, typeDict)[0]
+
+	for i in range(depth, 0, -1):
+		writeString += "\t" * i + "}\n"
+
+	return (writeString, False)
 
 def writeSupportFuncs(functionList):
 	writeString = "void getFunctionNamefromStream();\n\n"
@@ -362,83 +576,16 @@ def writeSupportFuncs(functionList):
 	writeString += "\t\tthrow C150Exception(\"%s: method name not null terminated or too long\");\n}" % fileStub[:-4]
 	return writeString
 
-def intToArgType(argName, first):
+def readNDArray(depth, argType, argName, typeDict):
 	writeString = ""
-	if first:
-		writeString += "\t%s = stoi(string(readBuffer));\n" % (argName)
-		writeString += "\treadLen += to_string(%s).length()+1;\n" % (argName)
-		first = False
-	else:
-		writeString += "\t%s = stoi(string(&(readBuffer[readLen])));\n" % (argName)
-		writeString += "\treadLen += to_string(%s).length()+1;\n" % (argName)
-	return (writeString, first)
-
-def floatToArgType(argName, first):
-	writeString = ""
-	if first:
-		writeString += "\t%s = stof(string(readBuffer));\n" % (argName)
-		writeString += "\treadLen += to_string(%s).length()+1;\n" % (argName)
-		first = False
-	else:
-		writeString += "\t%s = stof(string(&(readBuffer[readLen])));\n" % (argName)
-		writeString += "\treadLen += to_string(%s).length()+1;\n" % (argName)
-	return (writeString, first)
-
-def stringToArgType(argName, first):
-	writeString = ""
-	if first:
-		writeString += "\t%s = readBuffer;\n" % (argName)
-		writeString += "\treadLen += %s.length()+1;\n" % (argName)
-		first = False
-	else:
-		writeString += "\t%s = &(readBuffer[readLen]);\n" % (argName)
-		writeString += "\treadLen += %s.length()+1;\n" % (argName)
-	return (writeString, first)
-
-def stubCreateArg(arg, typeDict):
-	writeString = ""
-	typeOfType = typeDict[arg["type"]]["type_of_type"]
-	if typeOfType == "builtin":
-		writeString += "\t%s %s;\n" % (arg["type"], arg["name"])
-	elif typeOfType == "array":
-		memberType = typeDict[arg["type"]]["member_type"]
-		elementCount = typeDict[arg["type"]]["element_count"]
-		writeString += "\t%s %s[%s];\n" % (memberType, arg["name"], str(elementCount))
-	elif typeOfType == "struct":
-		print "struct"
+	iterVar = 'i'
+	for i in range(depth):
+		elementCount = typeDict[argType]["element_count"]
+		writeString += '\t' * (i + 1)
+		writeString += "for (int %s=0; %s<%s; %s++) {\n" % (iterVar, iterVar, elementCount, iterVar)
+		iterVar = chr(ord(iterVar) + 1)
+		argType = typeDict[argType]["member_type"]
 	return writeString
-
-def arrayToArgType(first, arg, typeDict):
-	if first:
-		first = False
-
-	writeString = ""
-	typeObj = typeDict[arg["type"]]
-	memberType = typeObj["member_type"]
-
-	writeString += "\tfor (int i=0; i<%s; i++) {\n" % typeObj["element_count"]
-	writeString += "\t\ttemp = string(&readBuffer[readLen]);\n"
-
-	if memberType == "int" or memberType == "float":
-		writeString += "\t\ttry {\n" 
-		if memberType == "int":
-			writeString += "\t\t\t%s[i] = stoi(temp);\n" % arg["name"]
-			writeString += "\t\t} catch (invalid_argument&) {\n"
-			writeString += "\t\t\tcerr << \"Problem with stoi'ing\" << endl;\n"
-		else:
-			writeString += "\t\t\t%s[i] = stof(temp);\n" % arg["name"]
-			writeString += "\t\t} catch (invalid_argument&) {\n"
-			writeString += "\t\t\tcerr << \"Problem with stof'ing\" << endl;\n"
-		writeString += "\t\t}\n"
-	elif memberType == "string":
-		writeString += "\t\t%s[i] = temp;\n" % arg["name"]
-	
-	writeString += "\t\treadLen += temp.length()+1;\n"
-	writeString += "\t}\n"
-	first = False
-
-	return (writeString, first)
-
 
 if __name__ == "__main__":
 		 
